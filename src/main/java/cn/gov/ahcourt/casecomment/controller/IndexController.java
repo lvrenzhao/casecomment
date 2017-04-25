d7c47ff7c4abee7b8bc20d91854e780223c93a2a
lvrenzhao <5306735@qq.com>
2017-04-25 12:27:01 +0800

    test222

--- src/main/java/cn/gov/ahcourt/casecomment/controller/IndexController.java
+++ src/main/java/cn/gov/ahcourt/casecomment/controller/IndexController.java
@@ -1,6 +1,9 @@
 package cn.gov.ahcourt.casecomment.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import cn.gov.ahcourt.casecomment.bean.MenuBean;
@@ -15,8 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
 
 @Controller
 public class IndexController {
@@ -46,15 +52,77 @@ public class IndexController {
         return "casecheck/check/player";
     }
 
-    public void initMenusByRole(List<MenuBean> menus,UserBean user){
-        for(int i = 0 ; menus!=null && i< menus.size(); i++){
-            if(StringUtils.isEmpty(menus.get(i).getCdurl())){
-                List<MenuBean> submenus = menuService.getMenusByParentID(menus.get(i).getCdid(), user.getJsid());
-                menus.get(i).setSubmenus(submenus);
-                initMenusByRole(submenus,user);
+    @RequestMapping(value = "splayer", method = RequestMethod.GET)
+    public void splayer(final HttpServletRequest request,final HttpServletResponse response) throws IOException {
+
+        String filename = request.getSession().getServletContext().getRealPath("/assets/movie.mp4");
+        File file = new File(filename);
+
+        int length = (int)file.length();
+        int start = 0;
+        int end = length - 1;
+
+        String range = request.getHeader("Range");
+        System.out.println(range);
+
+        int contentLength = end - start + 1;
+
+        response.reset();
+        response.setBufferSize(4 * 1024);
+        response.setHeader("Content-Disposition",
+                String.format("inline;filename=\"%s\"", "movie.mp4"));
+        response.setHeader("Accept-Ranges", "bytes");
+        response.setContentType("video/mp4");
+        response.setHeader("Content-Range",
+                String.format("bytes %s-%s/%s", start, end, length));
+        response.setHeader("Content-Length", String.format("%s", contentLength));
+        response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+
+        InputStream in = null;
+        ServletOutputStream out = null;
+        try {
+            in = new FileInputStream(file);
+            out = response.getOutputStream();
+            byte[] buffer = new byte[4 * 1024];
+            int t_length;
+            while ((t_length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, t_length);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("文件读取失败,文件不存在");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("文件流输出异常");
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+            } catch (IOException e) {
+                System.out.println("文件流关闭异常");
+                e.printStackTrace();
             }
         }
+
+
+//        int bytesRead;
+//        int bytesLeft = contentLength;
+//        ByteBuffer buffer = ByteBuffer.allocate(8192);
+//
+//        try (SeekableByteChannel = Files.newByteChannel(video, READ);
+//             OutputStream output = response.getOutputStream()) {
+//
+//            input.position(start);
+//
+//            while ((bytesRead = input.read(buffer)) != -1 && bytesLeft > 0) {
+//                buffer.clear();
+//                output.write(buffer.array(), 0,
+//                        bytesLeft < bytesRead ? bytesLeft : bytesRead);
+//                bytesLeft -= bytesRead;
+//            }
+//        }
     }
+
 
     @RequestMapping(value = "/userlist", method = { RequestMethod.GET, RequestMethod.POST })
     public @ResponseBody Map userlist(@SessionScope("user") UserBean yh, UserBean user) {
@@ -63,4 +131,13 @@ public class IndexController {
         return map;
     }
 
+    private void initMenusByRole(List<MenuBean> menus,UserBean user){
+        for(int i = 0 ; menus!=null && i< menus.size(); i++){
+            if(StringUtils.isEmpty(menus.get(i).getCdurl())){
+                List<MenuBean> submenus = menuService.getMenusByParentID(menus.get(i).getCdid(), user.getJsid());
+                menus.get(i).setSubmenus(submenus);
+                initMenusByRole(submenus,user);
+            }
+        }
+    }
 }
