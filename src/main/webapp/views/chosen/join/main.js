2e962dac6207efd7bfe0c74b3c32955cb8c2148d
lvrenzhao <5306735@qq.com>
2017-08-19 07:31:07 +0800

    hooah hooah hooah

--- src/main/webapp/views/chosen/join/main.js
+++ src/main/webapp/views/chosen/join/main.js
@@ -85,6 +85,64 @@ $(function () {
         pager : '#pager1'
         ,viewrecords: true
     });
+
+    $("#table2").jqGrid({
+        url : ahcourt.ctx + '/assets/data/casecheck_notice_verify_table1.json',
+        datatype : "json",
+        mtype : "post",
+        height : $('body').height() -200,
+        width : $('body').width() - 22,
+        rownumbers : true,
+        shrinkToFit : true,
+        rowNum : 20,
+        rowList : [ 10, 20, 30 ],
+        colModel : [ {
+            label : 'id',
+            name : 'id',
+            hidden : true,
+            key : true
+        },{
+            label : '案号',
+            name : 'pclx',
+            width : 120
+        }, {
+            label : '是否被评选',
+            name : 'shsj',
+            width : 100
+        }, {
+            label : '评选得分',
+            name : 'shsj',
+            width : 100
+        }, {
+            label : '未评选原因',
+            name : 'shsj',
+            width : 100,
+            formatter:function (cellvalue,options,rowObject) {
+                return '<a href="javascript:;" onclick="ViewComment(\'' + rowObject.ggid + '\')">'+cellvalue+'</a>'
+            }
+        },{
+            label : '归属法院',
+            name : 'lxrmc',
+            width : 100
+        }, {
+            label : '承办部门',
+            name : 'fbsj',
+            width : 100
+        }, {
+            label : '承办人',
+            name : 'fbsj',
+            width : 100
+        }, {
+            label : '推荐理由',
+            name : 'shr',
+            width : 300,
+            formatter:function (cellvalue,options,rowObject) {
+                return '<a href="javascript:;" onclick="ViewComment(\'' + rowObject.ggid + '\')">'+cellvalue+'</a>'
+            }
+        } ],
+        pager : '#pager2'
+        ,viewrecords: true
+    });
 });
 
 
@@ -127,6 +185,21 @@ function joinCase(key) {
         shade : 0.3,
         area : [ '600px', '420px' ],
         content : ahcourt.ctx + '/views/chosen/join/add_case.jsp?ggid=' + key,
+        cancel : function(index) {
+            layer.close(index);
+        }
+    });
+}
+
+function ViewComment(content) {
+    layer.open({
+        type : 1,
+        shift : 5,
+        title : "查看推荐理由",
+        shadeClose : false,
+        shade : 0.3,
+        area : [ '400px', '280px' ],
+        content : content,
         cancel : function(index) {
             layer.close(index);
         }
