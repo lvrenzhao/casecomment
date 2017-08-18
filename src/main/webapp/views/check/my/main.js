79c417f4d527a01d050de89ac6c6b8e9b99066de
lvrenzhao <5306735@qq.com>
2017-08-18 09:54:24 +0800

    hooah hooah hooah

--- src/main/webapp/views/check/my/main.js
+++ src/main/webapp/views/check/my/main.js
@@ -2,5 +2,133 @@
 
 //============================================================
 $(function () {
-
+    loadGrid();
 });
+
+function loadGrid() {
+    $("#table1").jqGrid({
+        url : ahcourt.ctx + '/assets/data/casecheck_notice_verify_table1.json',
+        datatype : "json",
+        mtype : "post",
+        height : $('body').height() -190,
+        width : $('body').width() - 10,
+        rownumbers : true,
+        shrinkToFit : false,
+        rowNum : 20,
+        rowList : [ 10, 20, 30 ],
+        colModel : [ {
+            label : 'ajid',
+            name : 'ajid',
+            hidden : true,
+            key : true,
+            frozen:true
+        }, {
+            label : 'ggid',
+            name : 'ggid',
+            hidden : true,
+            frozen:true
+        },{
+            label : '操作',
+            name : 'ggid',
+            width : 180,
+            align : 'center',
+            sortable : false,
+            formatter : function(cellvalue, options, rowObject) {
+                return '<button class="btn btn-link btn-xs " type="button" onclick="comment(2,\'' + cellvalue + '\')" title="点评"><i class="fa fa-info"></i> 评查详情</button>'
+                    +      '<button class="btn btn-link btn-xs " type="button" onclick="check(2,\'' + cellvalue + '\')" title="评查"><i class="fa fa-dedent"></i> 案件资料</button>';
+            },
+            frozen:true
+        },{
+            label : '案号',
+            name : 'xmmc',
+            frozen : true,
+            width : 150,
+            frozen:true
+        }, {
+            label : '所属评查公告',
+            name : '',
+            width : 200
+        },{
+            label : '质量等级',
+            name : '',
+            width : 100
+        },{
+            label : '评查时间',
+            name : '',
+            width : 100
+        },{
+            label : '归属法院',
+            name : 'gsfy',
+            width : 100
+        }, {
+            label : '承办部门',
+            name : 'htmc',
+            width : 100
+        }, {
+            label : '承办人',
+            name : 'xmlxmc',
+            width : 80
+        }, {
+            label : '性质',
+            name : 'zylbmc',
+            width : 80
+        },{
+            label : '类型',
+            name : 'zylbmc',
+            width : 80
+        },  {
+            label : '案由',
+            name : 'xmfzrmc',
+            width : 150
+        }, {
+            label : '结案方式',
+            name : 'xmjlmc',
+            width : 80
+        }, {
+            label : '结案时间',
+            name : 'xmcymc',
+            width : 80
+        },{
+            label : '评查组长',
+            name : 'xmcymc',
+            width : 80
+        },{
+            label : '评查组员',
+            name : 'xmcymc',
+            width : 200
+        }],
+        pager : '#pager1'
+        ,viewrecords: true
+    }).jqGrid('setFrozenColumns');
+}
+
+
+function check(mode,ajid) {
+    layer.open({
+        type : 2,
+        shift : 5,
+        title : '案件评查',
+        shadeClose : false,
+        shade : 0.3,
+        area : [ '90%', '90%' ],
+        content : ahcourt.ctx + '/views/check/work/check.jsp?ajid=' + ajid+"&mode="+mode,
+        cancel : function(index) {
+            layer.close(index);
+        }
+    });
+}
+
+function comment(mode,ajid) {
+    layer.open({
+        type : 2,
+        shift : 5,
+        title : mode==1?'案件点评':"查看案件评查详情",
+        shadeClose : false,
+        shade : 0.3,
+        area : [ '90%', '90%' ],
+        content : ahcourt.ctx + '/views/check/work/comment.jsp?ajid=' + ajid+"&mode="+mode,
+        cancel : function(index) {
+            layer.close(index);
+        }
+    });
+}
--- src/main/webapp/views/check/my/main.jsp
+++ src/main/webapp/views/check/my/main.jsp
@@ -7,10 +7,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
     <!-- library list = slimscroll;metismenu;bsfileinput;icheck;jqgrid;laydate;layer;steps;ztree -->
-    <jsp:include page="/header.jsp?libs=" />
+    <jsp:include page="/header.jsp?libs=laydate;jqgrid;layer" />
     <script type="text/javascript" src="main.js"></script>
   </head>
   <body>
-  	
+  <div class="bmbox_layout title_fixed clearfix">
+    <div class="bmbox_title">
+      <h5>由我发起的评查案件</h5>
+      <div class="bmbox_tool">
+      </div>
+    </div>
+    <div class="bmbox_content clearfix">
+      <div class="form_center clearfix">
+        <div class="form_item wb20 fl">
+          <label>公告标题</label>
+          <input type="text" id="khmc" class="form-control input-sm" placeholder="请输入公告标题" maxlength="255" />
+        </div>
+        <div class="form_item wb15 fl">
+          <label>评查类型</label>
+          <select class="form-control input-sm">
+            <option value="">--请选择--</option>
+            <option value="">常规评查</option>
+          </select>
+        </div>
+        <div class="form_item wb15 fl">
+          <label>评查任务</label>
+          <select class="form-control input-sm">
+            <option value="">--请选择--</option>
+            <option value="">常规评查</option>
+          </select>
+        </div>
+
+        <div class="form_item wb25 fl">
+          <label>发布日期</label>
+          <div>
+            <input type="text" id="" class="form-control input-sm wb40 fl" placeholder="" maxlength="255" onclick="laydate({istime: true,format: 'YYYY/MM/DD'})" />
+            <span style="display: inline-block;float: left;padding: 5px 10px 0px 10px;">~</span>
+            <input type="text" id="" class="form-control input-sm wb40 fl" placeholder="" maxlength="255" onclick="laydate({istime: true,format: 'YYYY/MM/DD'})" />
+          </div>
+
+        </div>
+        <div class="form_item wb10 fl">
+          <button id="" class="btn btn-primary btn-sm" type="button" style="margin-top: 30px;"><i class="fa fa-search"></i> 查询</button>
+        </div>
+      </div>
+      <div>
+        <table id="table1" class="table table-striped"></table>
+        <div id="pager1"></div>
+      </div>
+    </div>
+  </div>
   </body>
 </html>
