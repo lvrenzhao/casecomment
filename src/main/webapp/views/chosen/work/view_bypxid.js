cf65cfac68eac6b74593725c730c377a9ee58ef5
lvrenzhao <5306735@qq.com>
2017-08-19 23:28:23 +0800

    hooah hooah hooah

--- src/main/webapp/views/chosen/work/view_bypxid.js
+++ src/main/webapp/views/chosen/work/view_bypxid.js
@@ -1,2 +1,110 @@
 //mode:1 = 无评选结果，评选组长，评选组员 ,无按照专家组进行过滤的条件。
 //mod:2 = 有评选结果，有评选组长，评选组员，有按照专家组进行过滤的条件
+
+var mode;
+$(function () {
+    mode = $.getUrlParam("mode");
+    loadGridCases();
+    loadGridGroup();
+});
+
+
+function loadGridCases() {
+    $("#table1").jqGrid({
+        url : ahcourt.ctx + '/assets/data/casecheck_notice_verify_table1.json',
+        datatype : "json",
+        mtype : "post",
+        multiselect : true,
+        height : $('body').height() - 145,
+        width : $('body').width() * 0.65-10,
+        rownumbers : true,
+        shrinkToFit : false,
+        rowNum : 100000000,
+        colModel : [
+            {label : 'ajid',name : 'ajid',hidden : true,key : true,sortable:false,frozen : true},
+            {label : 'ggid',name : 'ggid',hidden : true,sortable:false,frozen : true},
+            {label : '案号',name : 'xmmc', width : 120,sortable:false,frozen : true},
+            {label : '关联案件',name : '',width : 80,sortable:false,formatter:function (cellvalue,options,rowObject) {
+                return '<a onclick="viewRelated(\'' + cellvalue + '\')" href="javascript:;">'+cellvalue+'</a>';
+            }},{
+                label : '评选得分',
+                name : 'shsj',
+                width : 100
+            }, {
+                label : '评选时间',
+                name : 'lxrmc',
+                width : 100
+            },{
+                label : '填报日期',
+                name : 'fbsj',
+                width : 100
+            }, {
+                label : '推荐理由',
+                name : 'shr',
+                width : 300,
+                formatter:function (cellvalue,options,rowObject) {
+                    return '<a href="javascript:;" onclick="ViewComment(\'' + rowObject.ggid + '\')">'+cellvalue+'</a>'
+                }
+            },
+            {label : '归属法院',name : 'xmzt',width : 150,sortable:false},
+            {label : '承办部门',name : 'htmc', width : 100,sortable:false},
+            {label : '承办人',name : 'xmlxmc',width : 80,sortable:false},
+            {label : '性质',name : 'zylbmc',width : 80 ,sortable:false},
+            {label : '类型',name : 'zylbmc',width : 80,sortable:false},
+            {label : '案由',name : 'xmfzrmc',width : 120,sortable:false},
+            {label : '结案方式',name : 'xmjlmc', width : 80,sortable:false},
+            {label : '结案时间',name : 'xmcymc', width : 80,sortable:false}
+        ]
+    }).jqGrid('setFrozenColumns');
+}
+
+function loadGridGroup() {
+    $("#table2").jqGrid({
+        url : ahcourt.ctx + '/assets/data/casecheck_notice_verify_table1.json',
+        datatype : "json",
+        mtype : "post",
+        height : $('body').height() - 125,
+        width : $('body').width() * 0.35,
+        shrinkToFit : true,
+        rowNum : 20,
+        colModel : [
+            {label : 'ajid',name : 'ajid',hidden : true,key : true,sortable:false,frozen : true},
+            {label : '组名',name : 'xmzt',width : 100,sortable:false},
+            {label : '组长',name : 'xmzt',width : 80,sortable:false},
+            {label : '组员',name : 'htmc', width : 200,sortable:false},
+            {label : '评查',name : 'htmc', width : 60,sortable:false},
+            {label : '占比',name : 'htmc', width : 60,sortable:false}
+        ],
+        pager:"#pager2"
+        // ,viewrecords: true
+    });//.jqGrid('setFrozenColumns');
+}
+
+function ViewComment(content) {
+    layer.open({
+        type : 1,
+        shift : 5,
+        title : "查看详细",
+        shadeClose : false,
+        shade : 0.3,
+        area : [ '400px', '280px' ],
+        content : content,
+        cancel : function(index) {
+            layer.close(index);
+        }
+    });
+}
+function viewRelated(key) {
+    layer.open({
+        type : 2,
+        shift : 5,
+        title : '查看关联案件',
+        shadeClose : false,
+        shade : 0.3,
+        area : [ '90%', '90%' ],
+        content : ahcourt.ctx + '/views/check/start/view_relatedcases.jsp?ajid=' + key,
+        cancel : function(index) {
+            layer.close(index);
+        }
+    });
+}
--- src/main/webapp/views/chosen/work/view_bypxid.jsp
+++ src/main/webapp/views/chosen/work/view_bypxid.jsp
@@ -7,17 +7,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>双评工作平台</title>
     <!-- library list = slimscroll;metismenu;bsfileinput;icheck;jqgrid;laydate;layer;steps;ztree -->
-    <jsp:include page="/header.jsp?libs=" />
+    <jsp:include page="/header.jsp?libs=jqgrid;layer" />
+    <script src="view_bypxid.js"></script>
 </head>
 <body>
-<div class="bmbox_layout wb35 clearfix fl">
+<div id="left" class="bmbox_layout wb35 clearfix fl">
     <div class="bmbox_title">
-        <h5>我的任务</h5>
+        <h5>专家组分配</h5>
         <div class="bmbox_tool">
+            <button class="btn btn-white btn-sm btn-smx" type="button"><i class="fa fa-refresh"></i> 清空选择</button>
         </div>
     </div>
     <div class="bmbox_content clearfix">
+        <table id="table2" class="table table-striped"></table>
+        <div id="pager2"></div>
+    </div>
+</div>
+<div id="right" class="bmbox_layout wb65 clearfix fl">
+    <div class="bmbox_title">
+        <h5>案件列表</h5>
+        <div class="bmbox_tool">
+        </div>
+    </div>
+    <div class="bmbox_content  clearfix ">
+        <div class="form_center clearfix" style="padding: 10px 0px;">
 
+            <div class="form_item wb40 fl">
+                <select class="form-control">
+                    <option value="">区域分布</option>
+                    <option value="1">合肥 - 共11件 - 占比25%</option>
+                </select>
+            </div>
+
+            <div class="form_item wb40 fl">
+                <select class="form-control">
+                    <option value="">案件性质分布</option>
+                    <option value="1">民事 - 共11件 - 占比25%</option>
+                </select>
+            </div>
+
+        </div>
+        <table id="table1" class="table table-striped"></table>
+        <div id="pager1"></div>
     </div>
 </div>
 </body>
