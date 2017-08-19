bb8643f77a6951e2daccc205ec330b7b6acc59b7
lvrenzhao <5306735@qq.com>
2017-08-19 20:38:31 +0800

    hooah hooah hooah

--- src/main/webapp/views/chosen/join/main.js
+++ src/main/webapp/views/chosen/join/main.js
@@ -201,7 +201,7 @@ function viewCases(mode,ggid) {
         shadeClose : false,
         shade : 0.3,
         area : [ '95%', '90%' ],
-        content : ahcourt.ctx + '/views/chosen/start/view_bypxid.jsp?ggid=' + ggid+"&mode="+mode,
+        content : ahcourt.ctx + '/views/chosen/work/view_bypxid.jsp?ggid=' + ggid+"&mode="+mode,
         cancel : function(index) {
             layer.close(index);
         }
