1bf094b601a0f1d359bcb62573c26077f8a169b8
lvrenzhao <5306735@qq.com>
2017-08-21 14:28:01 +0800

    hooah hooah hooah

--- src/main/webapp/views/check/reject/main.js
+++ src/main/webapp/views/check/reject/main.js
@@ -16,15 +16,92 @@ $(function () {
             label : 'ggid',
             name : 'ggid',
             hidden : true,
+            sortable:false,
             key : true
         },{
             label : 'btys',
             name : 'btys',
-            hidden : true
+            hidden : true,
+            sortable:false,
         },{
             label : 'sfyd',
             name : 'sfyd',
-            hidden : true
+            hidden : true,
+            sortable:false,
+        },{
+            label : '操作',
+            name : 'ggid',
+            width : 80,
+            align : 'center',
+            sortable : false,
+            formatter : function(cellvalue, options, rowObject) {
+                return '<button class="btn btn-link btn-xs " type="button" onclick="openCases(4,\'' + rowObject.ggid + '\')" title="查看公告详细"><i class="fa fa-info"></i> 公告详细</button>'
+            }
+        }, {
+            label : '公告标题',
+            name : 'ggbt',
+            width : 300,
+            sortable:false,
+            formatter : function(cellvalue, options, rowObject) {
+                var style = "";
+                if(rowObject.btys == 2){
+                    style = "color:red"
+                }
+                return '<span style="'+style+'">'+cellvalue+'</span>';
+            }
+        },{
+            label : '评查类型',
+            name : 'pclx',
+            align : 'center',
+            sortable:false,
+            width : 100
+        }, {
+            label : '评查任务',
+            name : 'pclx',
+            align : 'center',
+            sortable:false,
+            width : 100
+        },  {
+            label : '发布人',
+            name : 'fbsj',
+            sortable:false,
+            width : 100
+        }, {
+            label : '发布时间',
+            name : 'fbsj',
+            sortable:false,
+            width : 100
+        }],
+        pager : '#pager1'
+        ,viewrecords: true
+    });
+
+    $("#table2").jqGrid({
+        url : ahcourt.ctx + '/assets/data/casecheck_notice_verify_table1.json',
+        datatype : "json",
+        mtype : "post",
+        height : $('body').height() -125 ,
+        width : $('body').width() - 22,
+        rownumbers : true,
+        shrinkToFit : true,
+        rowNum : 20,
+        rowList : [ 10, 20, 30 ],
+        colModel : [ {
+            label : 'ggid',
+            name : 'ggid',
+            hidden : true,
+            key : true,
+            sortable:false,
+        },{
+            label : 'btys',
+            name : 'btys',
+            hidden : true,
+            sortable:false,
+        },{
+            label : 'sfyd',
+            name : 'sfyd',
+            hidden : true,
+            sortable:false,
         },{
             label : '操作',
             name : 'ggid',
@@ -39,6 +116,7 @@ $(function () {
             label : '公告标题',
             name : 'ggbt',
             width : 300,
+            sortable:false,
             formatter : function(cellvalue, options, rowObject) {
                 var style = "";
                 if(rowObject.btys == 2){
@@ -54,36 +132,42 @@ $(function () {
             label : '评查类型',
             name : 'pclx',
             align : 'center',
+            sortable:false,
             width : 100
         }, {
             label : '评查任务',
             name : 'pclx',
             align : 'center',
+            sortable:false,
             width : 100
         },  {
             label : '发布人',
             name : 'fbsj',
+            sortable:false,
             width : 100
         }, {
             label : '发布时间',
             name : 'fbsj',
+            sortable:false,
             width : 100
         }, {
             label : '审核人',
             name : 'shr',
+            sortable:false,
             width : 100
         } , {
             label : '审核时间',
             name : 'shsj',
+            sortable:false,
             width : 100
         }],
-        pager : '#pager1'
+        pager : '#pager2'
         ,viewrecords: true
     });
 });
 
-function reloadGrid() {
-    $("#table1").jqGrid().setGridParam({
+function reloadGrid2() {
+    $("#table2").jqGrid().setGridParam({
         url : ahcourt.ctx + '/assets/data/casecheck_notice_verify_table1.json',
         postData:{
         },
@@ -119,7 +203,7 @@ function openComments(ggid,shyj) {
         content : shyj,
         cancel : function(index) {
             layer.close(index);
-            reloadGrid();
+            reloadGrid2();
         }
 
     });
